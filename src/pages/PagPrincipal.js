0f8ef1191da430829061df12d31118f08372019b
angelitossj <angelito_cabj_2017@outlook.com>
2022-11-16 05:26:34 -0300

    subiendo las practicas con el useReducer

---
+++ src/pages/PagPrincipal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navegacion from '../components/Navegacion'
+const PagPrincipal = () => {
+  return (
+    <>
+    <Navegacion></Navegacion>
+    
+    </>
+  )
+}
+
+export default PagPrincipal
